2fc8f66f54d591696bda5d083b1e6d7cd89d9bc0
Lai-YT <381xvmvbib@gmail.com>
2022-02-07 21:54:42 +0800

    feat: introduce dynamic threshold maker

--- blink_detector.py
+++ blink_detector.py
@@ -1,8 +1,11 @@
 import math
 import statistics
+from collections import deque
+from decimal import Decimal
 from enum import Enum, auto
-from typing import Tuple
+from typing import Deque, Tuple, Union
 
+import numpy as np
 from imutils import face_utils
 from nptyping import Int, NDArray
 
@@ -10,6 +13,100 @@ from nptyping import Int, NDArray
 class EyeSide(Enum):
     LEFT  = auto()
     RIGHT = auto()
+
+
+class DynamicThresholdMaker:
+    """Take a certain number of landmarks that contains face to dynamically
+    determine a threshold of eye aspect ratio.
+
+    It can be used with a BlinkDetector to provide better detections on different
+    users.
+    "Decimal" module is used to reduce round-off errors, provide better precision.
+    """
+    def __init__(
+            self,
+            temp_thres: Union[Decimal, float] = Decimal('0.24'),
+            num_thres: int = 100) -> None:
+        """
+        Arguments:
+            temp_thres:
+                The temporary threshold used before the dynamic threshold is ready,
+                which prevents from getting an unreliable threshold due to low
+                number of samples. Should be in range [0.15, 0.5].
+            num_thres:
+                Higher number (included) of samples than this is considered to
+                be reliable.
+        """
+        if temp_thres < 0.15 or temp_thres > 0.5:
+            raise ValueError("resonable threshold should >= 0.15 and <= 0.5")
+        if num_thres < 100:
+            raise ValueError("number of samples under 100 makes the normal eye"
+                             "aspect ratio susceptible to extreme values")
+
+        self._dyn_thres = Decimal(temp_thres)
+        self._num_thres = num_thres
+        # To calculate the mean and standard deviation without re-summing all the
+        # samples, we store the real time sum and sum of squares.
+        self._cur_sum = Decimal(0)
+        self._cur_sum_of_sq = Decimal(0)
+        # The only reason we stored the sample ratios is to remove the old
+        # ratios in order.
+        self._samp_ratios: Deque[Decimal] = deque()
+
+    @property
+    def threshold(self) -> Decimal:
+        """Returns the dynamic threshold if the number of sample ratios
+        is enough; otherwise the temporary threshold.
+        """
+        return self._dyn_thres
+
+    def read_sample(self, landmarks: NDArray[(68, 2), Int[32]]) -> None:
+        """Reads the EAR value from the landmarks.
+
+        Notice that passing blinking landmarks makes the dynamic threshold
+        deviate, so avoid them if possible.
+        """
+        # Empty landmarks is not counted.
+        if not landmarks.any():
+            return
+
+        self.read_ratio(BlinkDetector.get_average_eye_aspect_ratio(landmarks))
+
+    def read_ratio(self, ratio: Union[Decimal, float]) -> None:
+        """Reads the EAR value directly.
+
+        Calculating EAR from landmarks or not is the only difference with
+        read_sample.
+
+        Notice that passing blinking ratios makes the dynamic threshold
+        deviate, so avoid them if possible.
+        """
+        self._update_sums(ratio)
+        self._update_dynamic_threshold()
+
+    def _update_sums(self, new_ratio: Union[Decimal, float]) -> None:
+        # add new ratio
+        new_ratio = Decimal(new_ratio)
+        self._samp_ratios.append(new_ratio)
+        self._cur_sum += new_ratio
+        self._cur_sum_of_sq += new_ratio * new_ratio
+        # remove old ratio
+        if len(self._samp_ratios) > self._num_thres:
+            old_ratio = self._samp_ratios.popleft()
+            self._cur_sum -= old_ratio
+            self._cur_sum_of_sq -= old_ratio * old_ratio
+
+    def _update_dynamic_threshold(self) -> None:
+        """Updates the dynamic threshold if the number of sample ratios
+        is enough.
+
+        Notte that the threshold = MEAN(sample EARs) - STD(sample EARs).
+        """
+        if len(self._samp_ratios) == self._num_thres:
+            mean = self._cur_sum / self._num_thres
+            mean_of_sq = self._cur_sum_of_sq / self._num_thres
+            std = (mean_of_sq - mean * mean).sqrt()
+            self._dyn_thres = mean - std
 
 
 class BlinkDetector:
