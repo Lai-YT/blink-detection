59e43b95089d6ddd823d23a7f32a227b532c72f2
Lai-YT <381xvmvbib@gmail.com>
2022-03-04 22:30:25 +0800

    feat: point-changing blink detection
    We see that the performance of using change point finding beats using
    dynamic threshold.

--- plot_ratios.py
+++ plot_ratios.py
@@ -17,21 +17,18 @@ class RatioPlotter:
     MISS_FACE_MARKER = "x"
     BLINK_MARKER = "*"
     # np.nan provides "skipping" effect when plotting
-    NON_BLINK_FILLER = np.nan
     MISS_FACE_FILLER = np.nan
 
     def __init__(self, output_dir: Path) -> None:
         self._ratios: List[Union[float]] = []
-        self._blinks: List[Union[float]] = []
-        self._thress: List[Union[float]] = []
-        self._means: List[Union[float]] = []
+        # frame numbers of blinks
+        self._blink_nos: List[Union[float]] = []
         self._output_dir = output_dir
 
     def read_samples_from(self, filename: str) -> None:
         self._filename = filename
         self._stem = Path(filename).stem
 
-        self._get_win_size_from_filename(fallback=500)
         self._clear_samples()
         self._update_samples()
         self._sample_size = len(self._ratios)
@@ -41,8 +38,8 @@ class RatioPlotter:
             self._parse_samples_from(f)
 
     def _parse_samples_from(self, fp: TextIO) -> None:
-        for line in fp:
-            self._curr_line_to_parse = line
+        for i, line in enumerate(fp):
+            self._line_to_parse = line
 
             if self._line_has_miss_face_marker():
                 self._append_miss_face_filler()
@@ -50,54 +47,33 @@ class RatioPlotter:
 
             if self._line_has_blink_marker():
                 self._remove_marker_from_line()
-                self._fill_ratio_back_to_blink()
-            self._append_non_blink_filler()
-            self._append_ratio_and_thres_from_line()
+                self._blink_nos.append(i)
+
+            # only ratio remaining
+            ratio = float(self._line_to_parse)
+            self._ratios.append(ratio)
 
     def _append_non_blink_filler(self) -> None:
-        self._blinks.append(self.NON_BLINK_FILLER)
+        self._blink_nos.append(self.NON_BLINK_FILLER)
 
     def _append_miss_face_filler(self) -> None:
         self._ratios.append(self.MISS_FACE_FILLER)
-        self._thress.append(self.MISS_FACE_FILLER)
-        self._blinks.append(self.MISS_FACE_FILLER)
-        self._means.append(np.nan)
-
-    def _append_ratio_and_thres_from_line(self) -> None:
-        ratio, thres, *mean = map(float, self._curr_line_to_parse.split())
-        self._ratios.append(ratio)
-        self._thress.append(thres)
-        if mean:
-            self._means.append(mean[0])
-        else:
-            self._means.append(np.nan)
-
-    def _fill_ratio_back_to_blink(self) -> None:
-        # consider the one before the last one the middle of blink
-        self._blinks[-2] = self._ratios[-2]
+        self._blink_nos.append(self.MISS_FACE_FILLER)
 
     def _line_has_miss_face_marker(self) -> bool:
-        return self._curr_line_to_parse.startswith(self.MISS_FACE_MARKER)
+        return self._line_to_parse.startswith(self.MISS_FACE_MARKER)
 
     def _line_has_blink_marker(self) -> bool:
-        return self._curr_line_to_parse.startswith(self.BLINK_MARKER)
-
-    def _is_miss_face_filler(self, x) -> bool:
-        return x == self.MISS_FACE_FILLER
-
-    def _is_non_blink_filler(self, x) -> bool:
-        return x == self.NON_BLINK_FILLER
+        return self._line_to_parse.startswith(self.BLINK_MARKER)
 
     def _remove_marker_from_line(self) -> None:
-        self._curr_line_to_parse = (
-            self._curr_line_to_parse.lstrip(self.BLINK_MARKER)
+        self._line_to_parse = (
+            self._line_to_parse.lstrip(self.BLINK_MARKER)
         )
 
     def _clear_samples(self) -> None:
         self._ratios.clear()
-        self._thress.clear()
-        self._blinks.clear()
-        self._means.clear()
+        self._blink_nos.clear()
 
     def plot(self, show: bool = True) -> None:
         fig, self._ax = plt.subplots()
@@ -105,10 +81,7 @@ class RatioPlotter:
         self._ax.set_title(f"{self._stem}; {std:.4f}")
 
         self._plot_ratios()
-        self._plot_rolling_stds()
-        # self._plot_thress()
-        # self._plot_means()
-        # self._plot_blinks()
+        self._plot_blinks()
         self._plot_annotate_blinks_if_exist()
         self._set_limit_and_ticks()
         self._ax.legend()
@@ -155,26 +128,12 @@ class RatioPlotter:
             label="ratio"
         )
 
-    def _plot_thress(self) -> None:
-        self._ax.plot(
-            np.arange(self._sample_size), self._thress,
-            color=mcolors.CSS4_COLORS["aqua"],
-            ls="--", linewidth=1.2,
-            label="thres"
-        )
-
-    def _plot_means(self) -> None:
-        self._ax.plot(np.arange(self._sample_size), self._means,
-            color=mcolors.CSS4_COLORS["lime"],
-            ls="--", linewidth=1.2,
-            label="mean"
-        )
-
     def _plot_blinks(self) -> None:
-        self._ax.scatter(
-            np.arange(self._sample_size), self._blinks,
-            color="g", alpha=0.5,
-            label="detect"
+        self._ax.vlines(
+            self._blink_nos,
+            ymin=0, ymax=1,
+            color=mcolors.CSS4_COLORS["indigo"],
+            alpha=0.5
         )
 
     def _plot_annotate_blinks_if_exist(self) -> None:
@@ -205,20 +164,9 @@ class RatioPlotter:
         return f"{self._get_stem_without_info_encoding()}_no.json"
 
     def _get_stem_without_info_encoding(self) -> str:
-        # The original stem of filename encode info to indicate the
-        # window size used in this specific detection.
         info = self._stem.split("_")
         # first 2 is the pure name
         return "_".join(info[:2])
-
-    def _get_win_size_from_filename(self, fallback: int) -> None:
-        info = self._stem.split("_")
-        win_size = fallback
-        for p in info:
-            if p.startswith("w"):
-                win_size = int(p.strip("w"))
-                break
-        self._win_size = win_size
 
 
 if __name__ == "__main__":
